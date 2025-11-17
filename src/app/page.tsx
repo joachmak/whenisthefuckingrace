96757405e58a01e599911d99fa315e23b74cd66d
joachmak <joachimmaksim@gmail.com>
2025-11-17 23:28:00 +0100

    refactor: remove redis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import MainWrapper from "@/app/components/MainWrapper";
 import {getEventsFromRawData, RawEventData} from "@/app/utils";
-import {getDataFromRedisStore, updateRedisStore} from "@/app/redis";
 import Main from "@/app/main";
 
 export default async function Home() {
     const currentYear = (new Date()).getUTCFullYear()
     const data = await fetch(`https://api.jolpi.ca/ergast/f1/${currentYear}`).catch(() => undefined)
-    let races: RawEventData[]
-    if (data?.ok) {
-        const json = await data.json()
-        races = json.MRData.RaceTable.Races
-        updateRedisStore(races)
-    } else {
-        races = await getDataFromRedisStore()
+
+    if (!data?.ok) {
+        throw new Error('Failed to fetch race data from F1 API')
     }
+
+    const json = await data.json()
+    const races: RawEventData[] = json.MRData.RaceTable.Races
     const events = getEventsFromRawData(races)
 
     return (
